8d062c2cd19bd44d1f9697e9660643ceecfa2da7
hbpen <huangbei@stu.pku.edu.cn>
2026-05-19 18:01:29 +0800

    Update project page

--- static/js/index.js
+++ static/js/index.js
@@ -36,6 +36,18 @@ $(document).ready(function() {
 			infinite: true,
 			autoplay: false,
 			autoplaySpeed: 3000,
+      breakpoints: [
+        {
+          changePoint: 768,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+        {
+          changePoint: 1080,
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      ],
     }
 
 		// Initialize all div with carousel class
